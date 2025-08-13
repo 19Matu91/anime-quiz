819403b876b04bbcf540b9dd3c4a3cd9f77f5872
A Matutano <alejandro.matutano@evoluciona.com>
2025-08-13 18:55:09 +0200

    noheader

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={DarkTheme}>
-      <Stack>
+      <Stack screenOptions={{
+        headerShown: false
+      }}>
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
