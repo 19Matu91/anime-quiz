a2caed91134a1a4ba752f0e079f529db6d31ca82
A Matutano <alejandro.matutano@evoluciona.com>
2025-08-16 14:11:17 +0200

    ultimos

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,8 @@
 import { Redirect } from 'expo-router';
-import { useEffect } from 'react';
 // import { useAuthStore } from '@/store/authStore';
 
 export default function Index() {
     //   const { isAuthenticated } = useAuthStore();
-    useEffect(() => {
-        console.log("eh")
-    }, [])
 
     //   return isAuthenticated ? 
     return false ?
